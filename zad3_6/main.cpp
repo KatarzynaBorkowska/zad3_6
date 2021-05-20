85e26ae1f57e98c7bd229b63ca359d7289bb5198
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-05-20 21:52:27 +0200

    Update main.cpp

--- zad3_6/main.cpp
+++ zad3_6/main.cpp
@@ -1,14 +1,26 @@
-//
-//  main.cpp
-//  zad3_6
-//
-//  Created by Kasia on 14/05/2021.
-//
+//Katarzyna Borkowska 7 Y1S1
+//zad 3_6
+//Funkcja (parametr a - napis o niezdefiniowanej długości) zwraca napis a którego wszystkie znaki zamieniono na duże litery.
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void zamian(char *a)
+{
+    
+    while(*a != NULL)
+    {
+        if(*a >= 'a' && *a <= 'z')
+            *a -= 32;
+        *a++;
+    }
+    
+}
+
+int main() {
+    char a[100];
+    cin >> a;
+    zamian(a);
+    cout << "Wynik: " << a << endl;
     return 0;
 }
